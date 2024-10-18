ccf64cbe4b3a791711e1a8e2ce4d2bf230edb88b
sidraamjed <sid.amjed87@gmail.com>
2024-10-18 08:59:02 +0200

    Create Dashy.py

---
+++ Dashy.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="OCD Symptom Severity Predictor",
+)
+
+# Sidebar configuration
+st.sidebar.image("./assets/OCD.jpg",)
+st.sidebar.success("Select a tab above.")
+
+# # Page information
+
+st.write("# Welcome to OCD Symptom Severity Predictor Dashboard!")
+
+st.markdown(
+"""
+    ## Aims ? Hello hello !! helloo hellooo whats up everyone
